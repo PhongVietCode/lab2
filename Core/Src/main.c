a863985479921de181eb86fc5e8d18d503e0ae15
PhongVietCode <quocphogn1101@gmail.com>
2023-10-06 22:54:47 +0700

    add ex2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -57,6 +57,32 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void switchLED(int n)
+{
+  switch (n)
+  {
+    {
+    case 0:
+      HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
+      HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
+      break;
+    case 1:
+      HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+      HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
+      break;
+    case 2:
+      HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+      HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
+      break;
+    case 3:
+      HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+      HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
+      break;
+    default:
+      break;
+    }
+  }
+}
 /* USER CODE END 0 */
 
 /**
@@ -90,11 +116,19 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
-  setTimer1(50);
+  setTimer1(100);
+  setTimer2(50);
   clearDisplay7Seg();
-  int num = 0;
+  display7Seg(1);
+  int num = 2;
+  int ledPos = 0;
+  HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_GPIO_Port, 0);
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0);
   HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
   HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
+  HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
+  HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -103,11 +137,16 @@ int main(void)
   {
     if (timer1_flag == 1)
     {
-      display7Seg(num % 9);
+      HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+      setTimer1(100);
+    }
+    if (timer2_flag == 1)
+    {
+      switchLED(ledPos % 4);
+      display7Seg(num % 4);
+      ledPos++;
       num++;
-      HAL_GPIO_TogglePin(EN0_GPIO_Port, EN0_Pin);
-      HAL_GPIO_TogglePin(EN1_GPIO_Port, EN1_Pin);
-      setTimer1(50);
+      setTimer2(50);
     }
     /* USER CODE END WHILE */
 
@@ -211,13 +250,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_Pin | EN0_Pin | EN1_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, DOT_Pin | LED_Pin | EN0_Pin | EN1_Pin | EN2_Pin | EN3_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, SEG0_Pin | SEG1_Pin | SEG2_Pin | SEG3_Pin | SEG4_Pin | SEG5_Pin | SEG6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_Pin EN0_Pin EN1_Pin */
-  GPIO_InitStruct.Pin = LED_Pin | EN0_Pin | EN1_Pin;
+  /*Configure GPIO pins : DOT_Pin LED_Pin EN0_Pin EN1_Pin
+                           EN2_Pin EN3_Pin */
+  GPIO_InitStruct.Pin = DOT_Pin | LED_Pin | EN0_Pin | EN1_Pin | EN2_Pin | EN3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
